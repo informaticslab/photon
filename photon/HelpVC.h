cc06287f0ae74840e9829a86e7374797e926f711
Greg Ledbetter <gsledbetter@gmail.com>
2014-04-03 17:55:39 -0400

    Check in start of work on long 508 help text.

--- photon/HelpVC.h
+++ photon/HelpVC.h
@@ -13,3 +13,48 @@
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @end
+
+NSString *iphone_help = @"Help for tabs: The Articles tab allows you to "
+@"View summaries of specific "
+@"MMWR articles. The Search tab allows you to search by subject to view "
+@"related article summaries. "
+
+@"Help for new content: Pull down to refesh content "
+@"within the Articles or Search "
+@"lists. Within the list of articles, a "
+@"blue dot appears before "
+@"unread articles. "
+
+@"Help for sharing: "
+@"Share a link to an article from "
+@"the Article Details screen by "
+@"tapping the Share icon "
+@"(located on the navigation "
+@" bar). The Article Details "
+@"screen is accessed by tapping "
+@"the Info icon (this is described later in Help) next "
+@"to the articleís name in the "
+@"Articles or Search lists. "
+
+@"To share the MMWR Express "
+@"app, tap the Share icon from "
+@"any other screen where it "
+@"appears. "
+
+@"Help for Info icon: Tapping the Info icon beside an article name in the "
+@"Articles or Search lists will "
+@"display the Article Details "
+@"screen. "
+
+@"The Article Details screen "
+@"has the full article name, "
+@"publication date, volume, and "
+@"number. You can also view "
+@"the full article text by tapping "
+@"the View Full Article button. If "
+@"you would like to share a link "
+@"to the article, tap the Share "
+@"icon (located on the screens "
+@" navigation bar). To learn "
+@"more about sharing, please "
+@"see above. ";
